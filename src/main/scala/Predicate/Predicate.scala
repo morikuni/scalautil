3cf2c880da7f27533ade7cf9b37a95295647f1f8
Taihei Morikuni <taihei.morikuni@gmail.com>
2015-06-24 23:45:12 +0900

    Remove unnecessary specification of type

--- src/main/scala/Predicate/Predicate.scala
+++ src/main/scala/Predicate/Predicate.scala
@@ -4,7 +4,8 @@ case class Predicate[A](run: A => Boolean){
 	def apply(a: A): Boolean = run(a)
 
 	def map(f: Boolean => Boolean): Predicate[A] = Predicate(a => f(run(a)))
-	def flatMap(f: Boolean => Predicate[A]): Predicate[A] = Predicate((a: A) => f(run(a)).run(a))
+
+	def flatMap(f: Boolean => Predicate[A]): Predicate[A] = Predicate(a => f(run(a)).run(a))
 
 	def &&(that: Predicate[A]): Predicate[A] = Predicate(a => {
 		val b = run(a)
